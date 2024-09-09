539e9f316ed47f6a8827eed7a6a3378960e8a06c
zerostrong <koheihaniwa129@gmail.com>
2024-09-09 20:26:16 +0900

    [update]条件分岐を修正

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ const userState = {};
 // userの路線データを保持
 const userRouteList = {};
 
+// 宣言した地域を保持
+var region;
+
 const routesByRegion = {
   '北海道': [
     '函館本線',
@@ -182,18 +185,17 @@ app.post('/webhook', (req, res) => {
       const userId = event.source.userId;
 
       // 地域選択状態
-      if (routesByRegion[message]) {
-        console.log('デバッグ1:', routesByRegion[message]);
+      if (routesByRegion[message] && userState[userId] === 'awaitingRegion') {
         const routeOptions = routesByRegion[message].join('\n');
         sendLineMessage(userId, `地域: ${message} \n登録したい路線を以下から教えてください。\n${routeOptions}`);
+        region = message;
         userState[userId] = 'awaitingRoute';
-        console.log('デバッグログ:', userState[userId]);
         } else {
         sendLineMessage(userId, '無効な地域が入力されました。以下のリストから地域を選択してください。\n・北海道\n・東北\n・関東\n・中部\n・近畿\n・中国\n・四国\n・九州');
         }
 
       // 路線選択状態
-      if (regionRoutes.includes(message)) {
+      if (routesByRegion[region].includes(message) && userState[userId] === 'awaitingRoute') {
         // userRouteListに路線データを追加
         userRouteList.push[message];
         sendLineMessage(userId, `路線: ${message} を登録しました。`);
@@ -201,7 +203,7 @@ app.post('/webhook', (req, res) => {
         } else {
         sendLineMessage(userId, '無効な路線が入力されました。');
         }
-        
+
       // ここにメッセージ処理を追加
       console.log('ユーザーからのメッセージ:', message);
     }
